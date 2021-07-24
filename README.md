ddbecdf87388ee184eb64d128f66cb9953afed8d
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-07-24 12:14:54 -0500

    README

---
+++ README.md
@@ -0,0 +1,2 @@
+## Demo
+![demo](https://res.cloudinary.com/dvhl6xkqf/image/upload/v1627146828/ventasJava/Screenshot_29_edm1l5.png)
--- Ventas/src/com/edflor/ventas/test/VentasTest.java
+++ Ventas/src/com/edflor/ventas/test/VentasTest.java
@@ -12,12 +12,14 @@ public class VentasTest {
         //Instancia productos
         Producto producto1 = new Producto("Camisa", 50.00); 
         Producto producto2 = new Producto("Pantalon", 100.00);
+        Producto producto3 = new Producto("Playera", 100.00);
         
         //Instancia orden
         Orden orden1 = new Orden();
         //Ejecucion metodo agregarProducto
         orden1.agregarProducto(producto1);
         orden1.agregarProducto(producto2);
+        orden1.agregarProducto(producto3);
         //Ejecucion metodo mostrarOrden
         orden1.mostrarOrden();
     }
