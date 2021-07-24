156e068f44af3060581f02b1ecd20756b4c7f09d
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-07-24 12:09:10 -0500

    Instancia y ejecucion de las clases producto y orden

---
+++ Ventas/src/com/edflor/ventas/test/VentasTest.java
@@ -0,0 +1,24 @@
+
+package com.edflor.ventas.test;
+
+import com.edflor.ventas.*;
+
+/**
+ *
+ * @author luis.flores.ramirez
+ */
+public class VentasTest {
+    public static void main(String[] args) {
+        //Instancia productos
+        Producto producto1 = new Producto("Camisa", 50.00); 
+        Producto producto2 = new Producto("Pantalon", 100.00);
+        
+        //Instancia orden
+        Orden orden1 = new Orden();
+        //Ejecucion metodo agregarProducto
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        //Ejecucion metodo mostrarOrden
+        orden1.mostrarOrden();
+    }
+}
